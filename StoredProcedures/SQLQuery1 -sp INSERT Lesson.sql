d4d9f7d26307e931e2fcdddcfd4873fa3a622cbf
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-02-27 19:39:02 +0800

    Home Work DONE

---
+++ StoredProcedures/SQLQuery1 -sp INSERT Lesson.sql
@@ -0,0 +1,23 @@
+--SQLQuery1 -sp INSERT Lesson.sql
+
+USE PV_521_Import;
+GO
+ALTER PROCEDURE sp_InsertLesson
+	
+	@group_name			NCHAR(10),
+	@discipline_name	AS NVARCHAR(150),
+	@teacher			AS NVARCHAR(50),
+	@start_date			AS DATE,
+	@time				AS TIME OUTPUT,
+	@spent				AS BIT
+	
+AS
+
+	IF NOT EXISTS(SELECT lesson_id FROM Schedule WHERE [date]=@start_date AND [time]=@time AND [group]=@group_name)
+BEGIN
+	INSERT INTO Schedule([group],discipline,teacher,[date],[time],spent)
+	VALUES(@group_name,@discipline_name,@teacher,@start_date,@time,IIF(@start_date<GETDATE(),1,0));
+	SET @time= DATEADD(MINUTE,95,@time);
+END
+
+
--- StoredProcedures/SQLQuery1-sp INSERT Schedule.sql
+++ StoredProcedures/SQLQuery1-sp INSERT Schedule.sql
@@ -1,7 +1,7 @@
 --SQLQuery1-sp INSERT Schedule.sq
 USE PV_521_Import;
 GO--Применить
-CREATE PROCEDURE sp_InsertScheduleStacionar
+ALTER PROCEDURE sp_InsertScheduleStacionar
 	@group_name				 NCHAR(10),
 	@discipline_name		 AS NVARCHAR(150),
 	@teacher_first_name		 AS NVARCHAR(50),
@@ -31,15 +31,18 @@ WHILE @lesson_number < @number_of_lessons
 BEGIN
 		--SET @time=@start_time;
 		DECLARE @time AS TIME	=	@start_time;
-		PRINT(FORMATMESSAGE(N'%i  %s  %s  %s',@lesson_number,CAST(@date AS VARCHAR(24)),DATENAME(WEEKDAY,@date),CAST(@time AS VARCHAR(24))));
-		IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [date]=@date AND [time]=@time AND [group]=@group)
-			INSERT Schedule VALUES(@group,@discipline,@teacher,@date,@time,IIF(@date<GETDATE(),1,0));
+		--PRINT(FORMATMESSAGE(N'%i  %s  %s  %s',@lesson_number,CAST(@date AS VARCHAR(24)),DATENAME(WEEKDAY,@date),CAST(@time AS VARCHAR(24))));
+		--IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [date]=@date AND [time]=@time AND [group]=@group)
+			--INSERT Schedule VALUES(@group,@discipline,@teacher,@date,@time,IIF(@date<GETDATE(),1,0));
+		EXEC sp_InsertLesson @group,@discipline,@teacher,@date,@time OUTPUT,NULL;
+
 		SET @lesson_number = @lesson_number + 1;
-		SET @time = DATEADD(MINUTE,95,@start_time);
+		--SET @time = DATEADD(MINUTE,95,@start_time);
 
 		PRINT(FORMATMESSAGE(N'%i  %s  %s  %s',@lesson_number,CAST(@date AS VARCHAR(24)),DATENAME(WEEKDAY,@date),CAST(@time AS VARCHAR(24))));
-		IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [date]=@date AND [time]=@time AND [group]=@group)
-			INSERT Schedule VALUES(@group,@discipline,@teacher,@date,@time,IIF(@date<GETDATE(),1,0));
+		--IF NOT EXISTS (SELECT lesson_id FROM Schedule WHERE [date]=@date AND [time]=@time AND [group]=@group)
+			--INSERT Schedule VALUES(@group,@discipline,@teacher,@date,@time,IIF(@date<GETDATE(),1,0));
+		EXEC sp_InsertLesson @group,@discipline,@teacher,@date,@time OUTPUT,NULL;
 		SET @lesson_number = @lesson_number + 1;
 
 		DECLARE @day AS TINYINT = DATEPART(WEEKDAY,@date);

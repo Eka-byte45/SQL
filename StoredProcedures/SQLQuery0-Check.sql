7cb3e3bd7f2c6b4462088e8a942aa5e58b89b9d6
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-03-12 02:52:59 +0800

    Alternate Schedule in progress

--- StoredProcedures/SQLQuery0-Check.sql
+++ StoredProcedures/SQLQuery0-Check.sql
@@ -3,21 +3,25 @@ USE PV_521_Import;
 
 SET DATEFIRST 1;
 --DELETE FROM Schedule WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE '%ADO.NET');
---DELETE FROM Schedule WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name = 'Сетевое программирование');
+--DELETE FROM Schedule WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name = 'Системное программирование');
 --EXEC sp_InsertScheduleStacionar N'PV_521',N'%ADO.NET%',N'Олег',N'2026-02-04';
---EXEC sp_InsertScheduleStacionar N'PV_521',N'%Сетевое%',N'Олег',N'2026-03-09';
---EXEC sp_InsertScheduleStacionar N'PV_521',N'%Системное%',N'Олег',DEFAULT;
---EXEC sp_InsertScheduleStacionar N'PV_521',N'%HTML/CSS%',N'Олег',DEFAULT;
+--EXEC sp_InsertScheduleStacionar N'PV_521',N'Сетевое%',N'Олег',N'2026-03-09';
+--EXEC sp_InsertScheduleStacionar N'PV_521',N'Системное%',N'Олег',DEFAULT;
+--EXEC sp_InsertScheduleStacionar N'PV_521',N'HTML/CSS',N'Олег',DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521',N'JavaScript',N'Олег',DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521',N'ReactJS',N'Олег',DEFAULT;
+--EXEC sp_InsertScheduleStacionar N'PV_521',N'%ASP.NET',N'Олег',DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521',N'',N'Олег',DEFAULT;
 --EXEC sp_InsertScheduleStacionar N'PV_521',N'%MS SQL Server',N'Олег',N'2025-12-24';
+EXEC sp_InsertSchedule1221 N'PV_521',N'Hardware%',N'Свищев',N'2025-01-20',1,3,5;
+EXEC sp_InsertSchedule1221 N'PV_521',N'%Windows',N'Свищев',N'2025-04-30',1,3,5;
+EXEC sp_InsertSchedule1221 N'PV_521',N'Процедурное%C++',N'Ковтун',N'2025-01-20',5,3,1;
 EXEC sp_SelectScheduleFor N'PV_521';
 
 --EXEC sp_InsertScheduleStacionar2 N'PV_521', N'Язык программирования C#', N'Олег', N'2026-06-01';
 --EXEC sp_SelectScheduleFor N'PV_521';
 --SELECT *FROM Disciplines;
---DELETE FROM Schedule WHERE [group]=521 AND [date]>=N'2026-05-18'
+--DELETE FROM Schedule WHERE [group]=521-- AND [date]>=N'2026-08-28'
 -----------------------------   Holidays   ------------------------
 --EXEC sp_InsertAllHolidaysFor 2027;
 --SELECT 
